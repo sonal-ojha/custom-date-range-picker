58f13cb171e501c64dae61e3ce33731e423a4f91
sonalsonu <sonal.ojha@moengage.com>
2019-06-09 20:33:53 +0530

    Added logic to get the Date range selection made from the date-picker

--- README.md
+++ README.md
@@ -22,6 +22,20 @@ import './App.css';
 import CustomDateRangePicker from 'custom-date-range-picker';
 
 class App extends Component {
+
+  selectedDateRange = (value, dates, label) => {
+    // Argument 1: value is the date that is displayed on selection like : 2 Jun - 9 Jun
+    // Argument 2: Return an Array of objects with length 2.
+        // Each Moment object has details such as follows: 
+        // _d: Sun Jun 02 2019 20:22:44 GMT+0530 (India Standard Time) 
+        // _isAMomentObject: true
+        // _isUTC: false
+        // _isValid: true
+        // _locale: P {_calendar: {…}, _longDateFormat: {…}, _invalidDate: "Invalid date", ordinal: ƒ, // _dayOfMonthOrdinalParse: /\d{1,2}(th|st|nd|rd)/, …}
+        // _pf: {empty: false, unusedTokens: Array(0), unusedInput: Array(0), overflow: -2, charsLeftOver: 0, …}
+    // Argument 3: Return Label Name of the selected option. For example Last 7 Days, Last 30 Days, Today ....(On selection of Last N Days the third parameter returns a Number that is entered)
+  }
+
   render() {
     return (
       <div className="App">
@@ -32,7 +46,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <CustomDateRangePicker />
+        <CustomDateRangePicker 
+          getDateRange={this.selectedDateRange}
+        />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import CustomDatePicker from './customDatePicker.component';
 import Moment from 'moment';
 import { Input } from 'semantic-ui-react';
+import PropTypes from 'prop-types';
 
 class CustomDateRangePicker extends Component {
     constructor(props){
@@ -14,7 +15,7 @@ class CustomDateRangePicker extends Component {
         }
     }
 
-  componentDidMount() {
+    componentDidMount() {
         const dateRange = [Moment().subtract(7, 'days'), Moment()];
         const date = `${Moment(dateRange[0]._d).format('D MMM')} - ${Moment(dateRange[1]._d).format('D MMM YY')}`;
         this.setState({
@@ -22,7 +23,7 @@ class CustomDateRangePicker extends Component {
         });
     }
 
-  handleDisplayDatePicker = () => {
+    handleDisplayDatePicker = () => {
 		const element = document.getElementById('customDatePicker');
 		const position = element.getBoundingClientRect();
 		const { displayDateRangePopup } = this.state;
@@ -31,55 +32,62 @@ class CustomDateRangePicker extends Component {
 			leftPosition: position.x,
 			topPosition: position.y,
 		});
-  }
+    }
 
-  handleDisplayDateRangePopup = (dates, displayPopup, dateRange, selectedLabel) => {
+    handleDisplayDateRangePopup = (dates, displayPopup, dateRange, selectedLabel) => {
+      const { getDateRange } = this.props;
 		this.setState({
 			dateSelected: dateRange,
 			displayDateRangePopup: displayPopup,
-		});
+        });
+        getDateRange(dateRange, dates, selectedLabel);
 	}
   
-  render() {
-    const { topPosition, leftPosition, displayDateRangePopup, dateSelected } = this.state;
-    const { maxNdays } = this.props;
-    return (
-        <div>
-            <div
-                style={{
-                    display: 'inline-block',
-                    backgroundColor: '#3a7e73',
-                    height: '30px',
-                    position: 'relative',
-                    borderBottomLeftRadius: '3px',
-                    borderTopLeftRadius: '3px',
-                }}
-            >
-                <div className="analytics-icons date-picker" style={{ display: 'inline-block' }} />
+    render() {
+        const { topPosition, leftPosition, displayDateRangePopup, dateSelected } = this.state;
+        const { maxNdays } = this.props;
+        return (
+            <div>
+                <div
+                    style={{
+                        display: 'inline-block',
+                        backgroundColor: '#3a7e73',
+                        height: '30px',
+                        position: 'relative',
+                        borderBottomLeftRadius: '3px',
+                        borderTopLeftRadius: '3px',
+                    }}
+                >
+                    <div className="analytics-icons date-picker" style={{ display: 'inline-block' }} />
+                </div>
+                <Input
+                    placeholder="Select date"
+                    value={dateSelected !== '' ? dateSelected : 'SELECT DATE'}
+                    onClick={this.handleDisplayDatePicker}
+                    id="customDatePicker"
+                    autoComplete="off"
+                />
+                {displayDateRangePopup
+                    && (
+                        <CustomDatePicker
+                            handleDisplayDateRangePopup={this.handleDisplayDateRangePopup}
+                            customContainerStyle={{
+                                top: topPosition,
+                                left: leftPosition,
+                            }}
+                            closeCustomDatePicker={this.closeCustomDatePicker}
+                            maxNdays={maxNdays}
+                        />
+                    )
+                }
             </div>
-            <Input
-                placeholder="Select date"
-                value={dateSelected !== '' ? dateSelected : 'SELECT DATE'}
-                onClick={this.handleDisplayDatePicker}
-                id="customDatePicker"
-                autoComplete="off"
-            />
-            {displayDateRangePopup
-                && (
-                    <CustomDatePicker
-                        handleDisplayDateRangePopup={this.handleDisplayDateRangePopup}
-                        customContainerStyle={{
-                            top: topPosition,
-                            left: leftPosition,
-                        }}
-                        closeCustomDatePicker={this.closeCustomDatePicker}
-                        maxNdays={maxNdays}
-                    />
-                )
-            }
-        </div>
-    );
-  }
+        );
+    }
+}
+
+CustomDateRangePicker.propTypes = {
+    getDateRange: PropTypes.func.isRequired,
+    maxNdays: PropTypes.number.isRequired,
 }
 
 export default CustomDateRangePicker;
